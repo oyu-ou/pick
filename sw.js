3327e0c24d18e1ce12ecdb825d196b08ff0f4d3b
oyu-ou <iamoyundari@gmail.com>
2026-03-05 13:39:13 +0000

    update

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener("install", event => {
-  self.skipWaiting();
+const CACHE_NAME = 'picker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/web-app-manifest-192x192.png',
+  '/web-app-manifest-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
