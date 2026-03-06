908bb97db1f947dde4fded7e961388341381a01a
oyu-ou <iamoyundari@gmail.com>
2026-03-06 08:26:34 +0000

    update

--- sw.js
+++ sw.js
@@ -7,26 +7,6 @@ const urlsToCache = [
   './web-app-manifest-512x512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return Promise.all(
-        urlsToCache.map(url =>
-          fetch(url).then(resp => {
-            if (!resp.ok) throw new Error(`${url} failed`);
-            return cache.put(url, resp);
-          }).catch(err => console.warn(err))
-        )
-      );
-    })
-  );
+self.addEventListener('install', () => {
+  console.log("service worker installed");
 });
